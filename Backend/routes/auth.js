8770ba434957e979d4bd9865a35946e6935cf511
RetchBlade <118535671+RetchBlade@users.noreply.github.com>
2024-06-11 12:50:28 +0200

    Fehler behoben

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -1,6 +1,6 @@
 // routes/auth.js
 const express = require('express');
-const User = require('@/models/User');
+const User = require('../models/User');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
--- Backend/routes/quiz.js
+++ Backend/routes/quiz.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const Question = require('@/models/Question');
+const Question = require('../models/Question');
 
 router.get('/questions', async (req, res) => {
   try {
-    const questions = await Question.find({}, { frage: 1, _id: 0 }); // Nur das Feld 'frage' abrufen, _id ausschließen
+    const questions = await Question.find({}, { frage, _id: 0 }); // Nur das Feld 'frage' abrufen, _id ausschließen
     res.json(questions);
   } catch (error) {
     res.status(500).json({ message: error.message });
